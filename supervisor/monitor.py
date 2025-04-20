280c5a8cfe75cfba38046f3a9881a7bfb111f40b
dmlinarr <mlidam@hotmail.com>
2025-04-20 19:35:22 +0200

    ignore SIGCHILD from monitor

--- supervisor/monitor.py
+++ supervisor/monitor.py
@@ -2,8 +2,23 @@ from sys import stderr, argv, exit
 from os import execv, kill, getpid
 from signal import SIGUSR1
 
-def main():
+from pyseccomp import SyscallFilter, KILL, ALLOW
+
+def seccomp(rule,syscall_name):
+    f = SyscallFilter(defaction=ALLOW)
     
+    if rule == "ALLOW":
+        # f.add_rule(ALLOW, syscall_name)
+        print(f"SECCOMP ALLOW {syscall_name}")
+    
+    if rule == "DENY":
+        # f.add_rule(KILL, syscall_name)
+        print(f"SECCOMP DENY {syscall_name}")
+ 
+    # load the filter into the kernel
+    f.load()
+
+def main():
     if len(argv) != 2:
         print("Nutzung: %s program" % argv[0], file=stderr)
         exit(1)
--- supervisor/supervisor.py
+++ supervisor/supervisor.py
@@ -1,8 +1,10 @@
+from signal import SIGUSR1, SIGCHLD, SIG_IGN, signal
+from sys import stderr, argv, exit
+from os import environ
+
 from ptrace.debugger import (PtraceDebugger,ProcessExit,NewProcessEvent)
 from ptrace.debugger.child import createChild
 from ptrace.func_call import FunctionCallOptions
-from signal import SIGUSR1
-from sys import stderr, argv, exit
 
 def ask_for_permission(syscall_formated):
     # TODO: Change here for communication whit user-tool using ZeroMQ
@@ -22,13 +24,17 @@ def main():
 
     # TODO: Wait for read_db, store it into the cache, put the path of the program into the message 
         
-    pid = createChild(arguments=['python3','supervisor/monitor.py']+argv[1:],no_stdout=False,env=None)
+    pid = createChild(
+        arguments=['python3','supervisor/monitor.py']+argv[1:],
+        no_stdout=True,
+        env=dict(environ)
+    )
     debugger = PtraceDebugger()
     process = debugger.addProcess(pid=pid, is_attached=True)
     process.cont()
     event = process.waitSignals(SIGUSR1)   
     process.syscall()
-        
+
     while True:
         try: 
             event = debugger.waitSyscall()
@@ -53,4 +59,5 @@ def main():
     debugger.quit()
     
 if __name__ == "__main__":
+    signal(SIGCHLD,SIG_IGN)
     main()
