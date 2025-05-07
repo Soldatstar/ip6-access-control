0042500627b1a08fccb67148888796b53ae58301
Soldatstar <h.logan.h@hotmail.com>
2025-05-07 09:24:15 +0200

    improves supervisor logging

--- supervisor/supervisor.py
+++ supervisor/supervisor.py
@@ -8,9 +8,28 @@ from multiprocessing import Manager, Process
 from itertools import chain
 from collections import Counter
 
-from ptrace.debugger import (PtraceDebugger,ProcessExit,NewProcessEvent,ProcessSignal)
+from ptrace.debugger import (
+    PtraceDebugger, ProcessExit, NewProcessEvent, ProcessSignal)
 from ptrace.func_call import FunctionCallOptions
 from pyseccomp import SyscallFilter, ALLOW, TRAP, Arg, EQ
+
+# Add the parent directory to sys.path
+import sys
+from pathlib import Path
+sys.path.append(str(Path(__file__).resolve().parent.parent))
+from shared import logging_config
+
+# Directories
+BASE_DIR = Path(__file__).resolve().parent.parent / "process-supervisor"
+LOGS_DIR = BASE_DIR / "logs"
+
+# Ensure required directories exist
+LOGS_DIR.mkdir(parents=True, exist_ok=True)
+
+# Configure logging
+log_file_path = LOGS_DIR / "supervisor.log"
+LOGGER = logging_config.configure_logging(log_file_path, "Supervisor")
+# TODO: improve logging, add various log levels
 
 PROGRAM_RELATIVE_PATH = None
 PROGRAM_ABSOLUTE_PATH = None
@@ -18,31 +37,37 @@ MANAGER = Manager()
 ALLOW_LIST = MANAGER.list()
 DENY_LIST = MANAGER.list()
 
+
 def init_seccomp(deny_list):
     f = SyscallFilter(defaction=ALLOW)
-    
+
     for deny_decision in deny_list:
         syscall_nr = deny_decision[0]
         for i in range(len(deny_decision[1:])):
-            try: 
+            try:
                 # TODO: Look at seccomp how path to files are handled
                 if not isinstance(deny_decision[1:][i], str):
-                    f.add_rule(TRAP, syscall_nr, Arg(i, EQ, deny_decision[1:][i]))
+                    f.add_rule(TRAP, syscall_nr, Arg(
+                        i, EQ, deny_decision[1:][i]))
             except TypeError as e:
-                print(f"TypeError: {e} - For syscall_nr: {syscall_nr}, Argument: {deny_decision[1:][i]}")
-        
+                LOGGER.info("TypeError: %s - For syscall_nr: %s, Argument: %s",
+                            e, syscall_nr, deny_decision[1:][i])
+
     f.load()
+
 
 def child_prozess(deny_list, argv):
     init_seccomp(deny_list=deny_list)
-    kill(getpid(),SIGUSR1)
-    execv(argv[1],[argv[1]]+argv[2:])
+    kill(getpid(), SIGUSR1)
+    execv(argv[1], [argv[1]]+argv[2:])
+
 
 def setup_zmq() -> zmq.Socket:
     context = zmq.Context()
     socket = context.socket(zmq.DEALER)
     socket.connect("tcp://localhost:5556")
     return socket
+
 
 def ask_for_permission_zmq(syscall_name, syscall_nr, arguments_raw, arguments_formated, socket) -> str:
     message = {
@@ -55,16 +80,20 @@ def ask_for_permission_zmq(syscall_name, syscall_nr, arguments_raw, arguments_fo
             "parameter_formated": arguments_formated
         }
     }
-    socket.send_multipart([b'', json.dumps(message).encode()])  
+    LOGGER.info("Asking for permission for syscall: %s", syscall_name)
+    socket.send_multipart([b'', json.dumps(message).encode()])
     while True:
         _, response = socket.recv_multipart()
         response_data = json.loads(response.decode())
+
         return response_data['data']['decision']
-    
+
+
 def set_program_path(relative_path):
     global PROGRAM_RELATIVE_PATH, PROGRAM_ABSOLUTE_PATH
     PROGRAM_RELATIVE_PATH = relative_path
     PROGRAM_ABSOLUTE_PATH = path.abspath(PROGRAM_RELATIVE_PATH)
+
 
 def init_shared_list(socket):
     global ALLOW_LIST, DENY_LIST
@@ -74,7 +103,8 @@ def init_shared_list(socket):
             "program": PROGRAM_ABSOLUTE_PATH
         }
     }
-    socket.send_multipart([b'', json.dumps(message).encode()])  
+    LOGGER.info("Initializing shared list with program path: %s", PROGRAM_ABSOLUTE_PATH)
+    socket.send_multipart([b'', json.dumps(message).encode()])
     while True:
         _, response = socket.recv_multipart()
         response_data = json.loads(response.decode())
@@ -85,16 +115,18 @@ def init_shared_list(socket):
                 syscall_args = syscall[1]
                 combined_array = [syscall_number] + syscall_args
                 ALLOW_LIST.append(combined_array)
-        
+
             for syscall in response_data['data']['denied_syscalls']:
                 syscall_number = syscall[0]
                 syscall_args = syscall[1]
                 combined_array = [syscall_number] + syscall_args
                 DENY_LIST.append(combined_array)
-
-            break    
-        elif response_data['status'] == "error":
+            LOGGER.info("Shared list initialized successfully.")
             break
+        elif response_data['status'] == "error":
+            LOGGER.info("Error initializing shared list: %s", response_data['data'])
+            break
+
 
 def is_already_decided(syscall_nr, arguments):
     for decision in chain(ALLOW_LIST, DENY_LIST):
@@ -103,11 +135,12 @@ def is_already_decided(syscall_nr, arguments):
                 return True
     return False
 
+
 def prepare_arguments(syscall_args):
     arguments = []
     for arg in syscall_args:
         match arg.name:
-            case "filename":  
+            case "filename":
                 arguments.append(arg.format())
             case "flags":
                 arguments.append(arg.value)
@@ -117,86 +150,92 @@ def prepare_arguments(syscall_args):
                 arguments.append("*")
     return arguments
 
+
 def main():
     if len(argv) < 2:
         print("Nutzung: %s program" % argv[0], file=stderr)
+        LOGGER.info("Nutzung: %s program", argv[0])
         exit(1)
-
+    LOGGER.info("Starting supervisor for %s", argv[1])
     set_program_path(relative_path=argv[1])
-    socket = setup_zmq() 
+    socket = setup_zmq()
     init_shared_list(socket=socket)
 
-    child = Process(target=child_prozess, args=(DENY_LIST,argv))
+    child = Process(target=child_prozess, args=(DENY_LIST, argv))
     debugger = PtraceDebugger()
     debugger.traceFork()
     child.start()
     process = debugger.addProcess(pid=child.pid, is_attached=False)
-     
+
     process.cont()
-    event = process.waitSignals(SIGUSR1)  
+    event = process.waitSignals(SIGUSR1)
     process.syscall()
-    
+
     while True:
-        try: 
+        try:
             event = debugger.waitSyscall()
             state = event.process.syscall_state
             syscall = state.event(FunctionCallOptions())
-    
+
             if syscall.result is None:
                 syscall_number = syscall.syscall
-                syscall_args = prepare_arguments(syscall_args=syscall.arguments)
-                syscall_args_formated = [arg.format() for arg in syscall.arguments]
+                syscall_args = prepare_arguments(
+                    syscall_args=syscall.arguments)
+                syscall_args_formated = [arg.format()
+                                         for arg in syscall.arguments]
                 combined_array = [syscall_number] + syscall_args
-                
-                if not is_already_decided(syscall_nr=syscall_number,arguments=syscall_args):
+
+                if not is_already_decided(syscall_nr=syscall_number, arguments=syscall_args):
                     decision = ask_for_permission_zmq(
                         syscall_name=syscall.name,
-                        syscall_nr=syscall_number, 
+                        syscall_nr=syscall_number,
                         arguments_raw=syscall_args,
-                        arguments_formated=syscall_args_formated, 
+                        arguments_formated=syscall_args_formated,
                         socket=socket
                     )
 
                     if decision == "ALLOW":
-                        print(f"Decision: ALLOW Syscall: {syscall.format()}")
+                        LOGGER.info("Decision: ALLOW Syscall: %s",
+                                    syscall.format())
                         ALLOW_LIST.append(combined_array)
-                    
+
                     if decision == "DENY":
-                        print(f"Decision: DENY Syscall: {syscall.format()}")
+                        LOGGER.info("Decision: DENY Syscall: %s",
+                                    syscall.format())
                         DENY_LIST.append(combined_array)
-                        process.setreg('orig_rax',-EPERM)
+                        process.setreg('orig_rax', -EPERM)
                         process.syscall()
                         debugger.waitSyscall()
-                        process.setreg('rax',-EPERM)
-                    
+                        process.setreg('rax', -EPERM)
+
             process.syscall()
 
-        except ProcessSignal as event: 
-            print(f"***SIGNAL***")
+        except ProcessSignal as event:
+            LOGGER.info("***SIGNAL***")
             event.display()
             process.syscall()
             continue
 
         except NewProcessEvent as event:
-            print("***CHILD-PROCESS***")
+            LOGGER.info("***CHILD-PROCESS***")
             # TODO: Observe the Child with the debugger
             subprocess = event.process
             subprocess.parent.syscall()
             continue
 
         except ProcessExit as event:
-            print("***PROCESS-EXECUTED***")
+            LOGGER.info("***PROCESS-EXECUTED***")
             break
 
         except KeyboardInterrupt:
-            print("Exiting supervisor...")
-            break 
-    
+            LOGGER.info("Exiting supervisor...")
+            break
+
     debugger.quit()
     child.join()
-    MANAGER.shutdown() 
+    MANAGER.shutdown()
     socket.close()
-    
-    
+
+
 if __name__ == "__main__":
     main()
